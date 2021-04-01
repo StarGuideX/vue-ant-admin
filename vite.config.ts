2ba3e3741c34b5763c79f8cb5c27a1534769f884
StarGuideX <gulei1011@hotmail.com>
2021-04-02 00:40:18 +0800

    解决ts报错

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import * as myImage from 'path'
+const {resolve} = require('path')
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+      vue()
+  ],
+  resolve:{
+    alias: [
+        {
+          find: '@', replacement: resolve(__dirname, 'src')
+        }
+      ]
+  }
+})
